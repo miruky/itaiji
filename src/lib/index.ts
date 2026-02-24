84fba81622af62a439e25cb3b77fc0f8a912e609
miruky <h.miyoki@gmail.com>
2026-02-24 15:58:46 +0900

    ライブラリの公開APIを整理

---
+++ src/lib/index.ts
@@ -0,0 +1,7 @@
+export { convert, modeLabels, normalizeVariants, toKyujitai, toShinjitai } from './convert';
+export type { ConvertResult, Mode, Segment } from './convert';
+export { allGroups, lookupChar } from './lookup';
+export type { CharInfo } from './lookup';
+export { KYU_TO_SHIN_ONLY, PAIRS } from './data/kyujitai';
+export { VARIANT_ENTRIES } from './data/variants';
+export type { VariantEntry } from './data/variants';
